abf3d5b224a5c608ac21f02d5ac0ce8dcca90195
leeggco <leeggco@gmail.com>
2019-08-13 22:39:15 +0800

    基础不见

--- lib/demo/basicDemo.dart
+++ lib/demo/basicDemo.dart
@@ -4,11 +4,61 @@ class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[100],
-      child: Row(children: <Widget>[
-        Container(
-          child: Icon(Icons.pool, size: 32.0, color: Colors.white,),
+      // color: Colors.grey[100],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('http://ww1.sinaimg.cn/large/007SS0VXgy1g5sne69wobj30m80goq6w.jpg'),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.indigoAccent[400].withOpacity(0.5),
+            BlendMode.hardLight,
+          )
         )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.pool, size: 32.0, color: Colors.white),
+            // color: Color.fromRGBO(3, 54, 255, 1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+            // 装饰
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              // 边框
+              border: Border.all(
+                color: Colors.indigoAccent[100],
+                width: 3.0,
+                style: BorderStyle.solid,
+              ),
+              // 圆角
+              // borderRadius: BorderRadius.circular(16.0),
+              // 阴影
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0.0, 16.0),
+                  color: Color.fromRGBO(16, 20, 188, 1.0),
+                  blurRadius: 25.0,
+                  spreadRadius: -9.0,
+                )
+              ],
+              // 形状
+              shape: BoxShape.circle,
+              // 渐变
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(7, 102, 255, 1.0),
+                  Color.fromRGBO(3, 28, 128, 1.0)
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              )
+            ),
+          )
       ],)
     );
     // return RichText(
